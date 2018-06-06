389f3327b68db2b9331e6b1cc9246197f38f2742
abhinash <abhi@macs-MacBook.local>
2018-06-05 20:05:43 -0400

    video_react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCIVoAc1pcwyWFrAfF4xHzqz_koPrOU1r0';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// create component. this component produces html
+/* functional component
+const App = () => {
+  return (
+  <div>
+    <SearchBar />
+  </div>
+);
+}
+*/
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      SelectedVideo: null
+    };
+    YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      //      this.setState({videos: videos})
+    });
+  }
+  render(){
+    return (
+    <div>
+      <SearchBar />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        onVideoSelect = { selectedVideo => this.setState({selectedVideo})}
+        videos = {this.state.videos} />
+    </div>
+  );
+}
+}
+
+//take this component and put it on the webpage
+ReactDOM.render(<App />, document.querySelector('.container'));
